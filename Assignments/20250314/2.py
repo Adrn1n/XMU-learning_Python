4ac0519f779311089bd2b75bb5f44f12f97481bb
Exxon <exxonliu001@yahoo.com>
2025-03-15 10:16:20 +0800

    20250315: Finish: 20250315: 2

--- Assignments/20250314/2.py
+++ Assignments/20250314/2.py
@@ -1,3 +1,22 @@
 """
 写一个程序输出元组中偶数和奇数的个数
 """
+
+n = int(input("n = "))
+A = []
+
+for i in range(n):
+    A.append(int(input()))
+
+T = tuple(A)
+
+oddN, evnN = 0, 0
+
+for num in T:
+    if num % 2:
+        oddN += 1
+    else:
+        evnN += 1
+
+print("Odd count:", oddN)
+print("Even count:", evnN)
