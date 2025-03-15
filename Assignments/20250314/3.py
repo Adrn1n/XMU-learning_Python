0fd9121f545aa80920de0190a58ba29399e819e2
Exxon <exxonliu001@yahoo.com>
2025-03-15 10:55:50 +0800

    20250315: +: 20250314: 3

--- Assignments/20250314/3.py
+++ Assignments/20250314/3.py
@@ -3,3 +3,16 @@
 (1) 矩阵行列由键盘输入
 (2) 矩阵元素的值为矩阵行列序号的乘积
 """
+
+m = int(input("m = "))
+n = int(input("n = "))
+
+A = []
+
+for i in range(m):
+    row = []
+    for j in range(n):
+        row.append((i + 1) * (j + 1))
+    A.append(row)
+
+print(A)
