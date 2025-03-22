ba7800434efc937c80e18071e711531180d1f087
Exxon <exxonliu001@yahoo.com>
2025-03-22 09:34:21 +0800

    20250322: +: 20250321/1

--- Assignments/20250321/1.py
+++ Assignments/20250321/1.py
@@ -1,3 +1,23 @@
 """
 写一个程序合并两个字典
 """
+
+dict1 = {}
+dict2 = {}
+
+n = int(input("n1:\n"))
+for i in range(n):
+    key = input("key:\n")
+    val = input("val:\n")
+    dict1[key] = val
+
+n = int(input("n2:\n"))
+for i in range(n):
+    key = input("key:\n")
+    val = input("val:\n")
+    dict2[key] = val
+
+dict_merged = dict1.copy()
+dict_merged.update(dict2)
+
+print(dict_merged)
